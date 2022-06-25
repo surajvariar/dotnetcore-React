54ac2755c4789912fbe1580d90d5425a727d0768
surajvariar <suraj.variar@gmail.com>
2022-06-25 13:43:47 +0530

    added code

---
+++ ReactDemo/wwwroot/js/tutorial.jsx
@@ -0,0 +1,9 @@
+﻿class CommentBox extends React.Component {
+    render() {
+        return (
+            <div className="commentBox">Hello, world! I am a Comment Box. Nice to see you</div>
+        );
+    }
+}
+
+ReactDOM.render(<CommentBox />, document.getElementById('content'));
